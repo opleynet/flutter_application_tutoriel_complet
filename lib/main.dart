e6b55831b6b3926a4c88cd11c5691818e18af916
Olivier Pleynet <olivier.pleynet@atos.net>
2026-03-01 15:12:39 +0100

    Add stateless and statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  //prend un widget et et construit l'application
   runApp(const MainApp());
 }
 
@@ -15,14 +16,44 @@ class MainApp extends StatelessWidget {
           title: Text("My app bar"),
           backgroundColor: Colors.yellow.shade200,
         ),
-        body: Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 30),
-            textAlign: TextAlign.center,
-          ),
-        ),
+        body: HomePage(),
       ),
     );
+  }
+}
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState(){
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage>{
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+          child: Row(
+            children: [
+              Text(
+                'Hello World!ss $counter',
+                style: TextStyle(fontSize: 30),
+                textAlign: TextAlign.center,
+              ),
+              FloatingActionButton(onPressed: (){
+                
+                setState(() {
+                  //noifier a flutter counter a changer sa valeur et relance le build
+                  counter +=1;
+                });
+              })
+            ],
+          ),
+        );
   }
 }
